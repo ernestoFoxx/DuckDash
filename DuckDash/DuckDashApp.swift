3d5b2868e66de00fbac89244e24a69777c4446ee
ernestoFoxx <65566637+ernestoFoxx@users.noreply.github.com>
2024-02-16 11:24:37 -0500

    Initial Commit

---
+++ DuckDash/DuckDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuckDashApp.swift
+//  DuckDash
+//
+//  Created by Ernesto Fox on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct DuckDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
